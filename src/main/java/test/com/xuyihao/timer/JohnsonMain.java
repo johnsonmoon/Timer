1c2692dca0f3928bbb2f1e22288ca9365acba87e
xuyh@broada.com <xuyh@uyunsoft.cn>
2016-08-08 19:20:50 +0800

    添加DateUtils以及TimeUtil工具类

--- src/main/java/test/com/xuyihao/timer/JohnsonMain.java
+++ src/main/java/test/com/xuyihao/timer/JohnsonMain.java
@@ -1,13 +1,21 @@
 package test.com.xuyihao.timer;
 
+import com.xuyihao.timer.util.TimeUtil;
+
 /**
  * Created by Xuyh at 2016/08/05 上午 11:37.
  */
 public class JohnsonMain {
-    public static void main(String args[]){
-        TestStaticCodeBlock.abcd();
-        while(true){
-
+   public static void main(String args[]){
+        while(true) {
+            long remainedTime = TimeUtil.getRemainedTimeMillis("00:00:01");
+            remainedTime = remainedTime/1000/60;
+            System.out.println(remainedTime);
+            try{
+                Thread.sleep(1000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
